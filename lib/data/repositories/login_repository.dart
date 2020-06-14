6796a363cc861cf3c2c8092d1bd4a9787aa8c993
Rijo George <>
2020-06-14 19:41:42 +0530

    repository refactor

--- lib/data/repositories/login_repository.dart
+++ lib/data/repositories/login_repository.dart
@@ -1,12 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
-import 'package:micro_social/infrastructure/network_info.dart';
-import '../data_sources/network/login_data_source.dart';
 
+import '../../common/exceptions.dart';
 import '../../common/failures.dart';
 import '../../domain/entities/user.dart';
-import '../../domain/repositories/dto/login_dto.dart';
 import '../../domain/repositories/abstract_login_repository.dart';
+import '../../domain/repositories/dto/login_dto.dart';
+import '../../infrastructure/network_info.dart';
+import '../data_sources/network/dataMapping/login_user_dto.dart';
+import '../data_sources/network/login_data_source.dart';
+
+typedef Future<LoginUserDTO> _LoginUsingEmailOrPassword();
 
 class LoginRepository implements AbstractLoginRepository {
   final LoginDataSource dataSource;
@@ -15,18 +19,27 @@ class LoginRepository implements AbstractLoginRepository {
 
   @override
   Future<Either<Failure, User>> loginUserUsingEmail(
-      LoginUsingEmailRequest request) async {
-    var isConnected = await networkInfo.isConnected;
-    if (isConnected == false){
-        return Left(ServerFailure());
-    }
-    final user =  await dataSource.loginUserUsingEmail(request);
-    return Right(user.toDomain());
-  }
+      LoginUsingEmailRequest request) async => await _loginUser(() {
+      return dataSource.loginUserUsingEmail(request);
+    });
 
   @override
   Future<Either<Failure, User>> loginUserUsingPhone(
-      LoginUsingPhoneRequest request) {
-    return null;
+      LoginUsingPhoneRequest request) async => await _loginUser(() {
+      return dataSource.loginUserUsingPhone(request);
+    });
+
+  Future<Either<Failure, User>> _loginUser(
+      _LoginUsingEmailOrPassword loginUser) async {
+    var isConnected = await networkInfo.isConnected;
+    if (isConnected == false) {
+      return Left(ServerFailure());
+    }
+    try {
+      final user = await loginUser();
+      return Right(user.toDomain());
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 }
